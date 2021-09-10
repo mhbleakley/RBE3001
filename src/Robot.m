cefd030667fa56e9a2b35a943e211cd2a9814057
Breiling <you@example.com>
2021-09-10 18:48:31 -0400

    added setpoint_cp as per issue 17
    Co-Authored by Rahil Parikh <rkparikh@wpi.edu>

--- src/Robot.m
+++ src/Robot.m
@@ -254,10 +254,17 @@ classdef Robot < handle
             T = self.fk3001(M);
         end
         
+        function T = setpoint_cp(self)
+            M = zeros(3,1);
+            N = zeros(2,3);
+            N = self.setpoint_js()*(2*pi/360);
+            M(1,1) = N(1,1);
+            M(2,1) = N(1,2);
+            M(3,1) = N(1,3);
+            T = self.fk3001(M);
+        end
 
 
-
-        
         
     end
 end
