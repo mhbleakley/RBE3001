3eb22152d2f6934b7dc9be85354d5db4cd6db6f4
Martin Bleakley <mhbleakley@wpi.edu>
2021-09-29 18:10:26 -0400

    finished plotting velocity vectors, completed part 4 of the lab
    Co-authored by Rahil Parikh <rkparikh@wpi.edu>
    Co-authored by Alexander Breiling

--- src/lab4.m
+++ src/lab4.m
@@ -34,81 +34,121 @@ try
   % packet sizes up to 64 bytes.
   packet = zeros(15, 1, 'single');
   
-  J = pp.jacob3001([0 90 -90]);
+  J = pp.jacob3001([0 90 -90])
   disp(det(J(1:3,:)));
   
   
    Pos1 = [50,55,75];
-   Pos2 = [50 0 35];
+   Pos2 = [75 -60 35];
    Pos3 = [140 60 90];
   
 % Linear Trajectory 
 
 i = 1; % counts iterations
-timestep = 1;
-x12traj = traj.linear_traj(Pos1(1,1), Pos2(1,1), 0, 1, timestep);
-x23traj = traj.linear_traj(Pos2(1,1), Pos3(1,1), 1, 2, timestep);
-x31traj = traj.linear_traj(Pos3(1,1), Pos1(1,1), 2, 3, timestep);
-
-y12traj = traj.linear_traj(Pos1(1,2), Pos2(1,2), 0, 1, timestep);
-y23traj = traj.linear_traj(Pos2(1,2), Pos3(1,2), 1, 2, timestep);
-y31traj = traj.linear_traj(Pos3(1,2), Pos1(1,2), 2, 3, timestep);
-
-z12traj = traj.linear_traj(Pos1(1,3), Pos2(1,3), 0, 1, timestep);
-z23traj = traj.linear_traj(Pos2(1,3), Pos3(1,3), 1, 2, timestep);
-z31traj = traj.linear_traj(Pos3(1,3), Pos1(1,3), 2, 3, timestep);
-
-
-timecount = 1;
-timecount2 = 1;
-timecount3 = 1;
 prevEndpoint = [-500; -500; -500; -500]; %initialize variable to impossible values so it never overlaps
-zeroVector = [0; 0; 0];
 AV = pp.measured_js(1,1); 
 fkAngle = transpose(AV(1, :));
 qVelocity = AV(2,:);
 endpoint = pp.fk3001(fkAngle)* [0; 0; 0; 1];
+
 tic
-while toc < 6
+while toc < 7
     
-    if ~(pp.finished_movement(transpose(endpoint(1:3,1)), Pos2)) && (timecount <= 1000)
+    if ~(pp.finished_movement(transpose(endpoint(1:3,1)), Pos2)) && (toc <= 2)
       
-          pp.interpolate_jp(pp.ik3001([x12traj(timecount), y12traj(timecount), z12traj(timecount)]), 0);
-          timecount = timecount + 1;
+        x12traj = traj.linear_traj(Pos1(1,1), Pos2(1,1), 0, 2, toc);  
+        y12traj = traj.linear_traj(Pos1(1,2), Pos2(1,2), 0, 2, toc);
+        z12traj = traj.linear_traj(Pos1(1,3), Pos2(1,3), 0, 2, toc);
+        pp.interpolate_jp(pp.ik3001([x12traj, y12traj, z12traj]),10);
       
-    elseif ~(pp.finished_movement(transpose(endpoint(1:3,1)), Pos3)) && (timecount2 <= 1000)
+    elseif ~(pp.finished_movement(transpose(endpoint(1:3,1)), Pos3)) && toc > 2 && toc < 4
 
-          pp.interpolate_jp(pp.ik3001([x23traj(timecount2), y23traj(timecount2), z23traj(timecount2)]), 0);
-          timecount2 = timecount2 + 1;
+        x23traj = traj.linear_traj(Pos2(1,1), Pos3(1,1), 2, 4, toc);
+        y23traj = traj.linear_traj(Pos2(1,2), Pos3(1,2), 2, 4, toc);
+        z23traj = traj.linear_traj(Pos2(1,3), Pos3(1,3), 2, 4, toc);
+        pp.interpolate_jp(pp.ik3001([x23traj, y23traj, z23traj]),10);
             
-    elseif ~(pp.finished_movement(transpose(endpoint(1:3,1)), Pos1)) && (timecount3 <= 1000)
+    elseif ~(pp.finished_movement(transpose(endpoint(1:3,1)), Pos1)) && toc > 4 && toc < 6
       
-          pp.interpolate_jp(pp.ik3001([x31traj(timecount3), y31traj(timecount3), z31traj(timecount3)]), 0);
-          timecount3 = timecount3 + 1;
-
+        x31traj = traj.linear_traj(Pos3(1,1), Pos1(1,1), 4, 6, toc);
+        y31traj = traj.linear_traj(Pos3(1,2), Pos1(1,2), 4, 6, toc);
+        z31traj = traj.linear_traj(Pos3(1,3), Pos1(1,3), 4, 6, toc);  
+        pp.interpolate_jp(pp.ik3001([x31traj, y31traj, z31traj]),10);
+   
     end
     
-    AV = pp.measured_js(1,1); 
+    AV = pp.measured_js(1,1);
     fkAngle = transpose(AV(1, :));
-    qVelocity = AV(2,:);
+    qVelocity = transpose(AV(2,:));
     endpoint = pp.fk3001(fkAngle)* [0; 0; 0; 1];
-    %if filters identical data and zero data out to remove noise
+    ee_jacob = pp.jacob3001(fkAngle);
+    ee_velocity = ee_jacob(1:3,1:3)*qVelocity;
+    pp.plot_arm(fkAngle,ee_velocity);
+    
     if(prevEndpoint(1,1) ~= endpoint(1,1) || prevEndpoint(2,1) ~= endpoint(2,1) || prevEndpoint(3,1) ~= endpoint(3,1) && 0 ~= endpoint(1,1) && 0 ~= endpoint(2,1) && 0 ~= endpoint(3,1))
         lin_traj_m(i, 1) = toc;
-        lin_traj_m(i, 2) = AV(1,1);
-        lin_traj_m(i, 3) = AV(1,2);
-        lin_traj_m(i, 4) = AV(1,3);
+        lin_traj_m(i, 2) = AV(2,1);
+        lin_traj_m(i, 3) = AV(2,2);
+        lin_traj_m(i, 4) = AV(2,3);
 
-        lin_traj_m(i,5) = endpoint(1,1);
-        lin_traj_m(i,6) = endpoint(2,1);
-        lin_traj_m(i,7) = endpoint(3,1);
+        lin_traj_m(i,5) = ee_velocity(1,1);
+        lin_traj_m(i,6) = ee_velocity(2,1);
+        lin_traj_m(i,7) = ee_velocity(3,1);
         i = i+1;
     end
+
     prevEndpoint = endpoint;
-%     pp.plot_arm(fkAngle);
-%     drawnow;
+    
 end
 
+writematrix(lin_traj_m,'linear_velocity_data.csv');
+filename = 'linear_velocity_data.csv';
+
+traj_data = csvread(filename);
+time = traj_data(:,1);
+
+% Position
+xVel = traj_data(:,5);
+yVel = traj_data(:,6);
+zVel = traj_data(:,7);
+
+xAngVel = traj_data(:,2);
+yAngVel = traj_data(:,3);
+zAngVel = traj_data(:,4);
+
+% ScalarVel = sqrt(((xVel(1,1)^2) + xVel(1,1)^2) + xVel(1,1)^2)) + (yVel^2) + (zVel^2));
+
+subplot(3,2,1)
+plot(time,xVel); 
+hold on
+plot(time,yVel);
+plot(time,zVel);
+hold off
+
+title("End Effector Linear Velocity)");
+xlabel('Time(s)') ;
+ylabel('Linear Velocity(mm/s)'); 
+legend('X Velocity', 'Y Velocity', 'Z Velocity');
+
+subplot(3,2,2)
+plot(time,xAngVel); 
+hold on
+plot(time,yAngVel);
+plot(time,zAngVel);
+hold off
+
+title("End Effector Angular Velocity)");
+xlabel('Time(s)') ;
+ylabel(' Angular Velocity(deg/s)'); 
+legend('X Angular Velocity', 'Y Angular Velocity', 'Z Angular Velocity');
+  
+subplot(3,2,3)
+plot(time,ScalarVel); 
+
+title("End Effector Scalar Linear Velocity)");
+xlabel('Time(s)') ;
+ylabel(' Scalar Linear Velocity(deg/s)'); 
+legend('Scalar Linear Velocity');
   
   catch exception
     getReport(exception)
@@ -118,4 +158,4 @@ end
 % Clear up memory upon termination
 pp.shutdown()
 
-% toc
+% tozc
